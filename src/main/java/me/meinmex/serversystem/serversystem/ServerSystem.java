41484a843f39dcfa991780164710a378a8118cf7
BuildTools <unconfigured@null.spigotmc.org>
2022-07-06 16:06:38 +0200

    [+] Help Command [+] Configuration for Help Command

--- src/main/java/me/meinmex/serversystem/serversystem/ServerSystem.java
+++ src/main/java/me/meinmex/serversystem/serversystem/ServerSystem.java
@@ -1,8 +1,11 @@
 package me.meinmex.serversystem.serversystem;
 
+import me.meinmex.serversystem.serversystem.commands.HelpCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class ServerSystem extends JavaPlugin {
 
@@ -30,7 +33,12 @@ public final class ServerSystem extends JavaPlugin {
         configuration.addDefault("ServerSystem.Prefix", "§8[§7System§8] §7|§r ");
         configuration.addDefault("ServerSystem.NoPermissions", "§8[§7System§8] §7|§r §cYou need the following permissions to use this command:§4 ");
 
+        configuration.addDefault("ServerSystem.Commands.Permission.Help", "ServerSystem.Help");
 
+        configuration.addDefault("ServerSystem.Commands.Messages.ArgsWrong.Help", "§cWrong arguments! Right: /help");
+        configuration.addDefault("ServerSystem.Commands.Messages.Help", "§aHelp not set yet!");
+
+        Objects.requireNonNull(getCommand("Help")).setExecutor(new HelpCommand());
         saveConfig();
     }
 
---
+++ src/main/java/me/meinmex/serversystem/serversystem/commands/HelpCommand.java
@@ -0,0 +1,29 @@
+package me.meinmex.serversystem.serversystem.commands;
+
+import me.meinmex.serversystem.serversystem.ServerSystem;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+import java.util.Objects;
+
+public class HelpCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        FileConfiguration configuration = ServerSystem.getInstance().configuration;
+        Player player = (Player) sender;
+        if (!player.hasPermission(Objects.requireNonNull(configuration.getString("ServerSystem.Commands.Permission.Help")))) {
+            player.sendMessage(configuration.getString("ServerSystem.Core.Prefex.NoPermissionsPrefix") + configuration.getString("ServerSystem.Commands.Permission.Help"));
+            return true;
+        }
+        if (!(args.length == 0)) {
+            player.sendMessage(configuration.getString("ServerSystem.Prefix") + configuration.getString("ServerSystem.Commands.Messages.ArgsWrong.Help"));
+            return true;
+        }
+        player.sendMessage(configuration.getString("ServerSystem.Prefix") + configuration.getString("ServerSystem.Commands.Messages.Help"));
+
+        return true;
+    }
+}
