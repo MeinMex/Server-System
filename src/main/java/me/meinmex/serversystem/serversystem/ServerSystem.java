48a3ea6048cfbddf25fd7d6e65b47290020c1d24
BuildTools <unconfigured@null.spigotmc.org>
2022-07-06 15:47:56 +0200

    [+] Enable Message [+] Disable Message

--- src/main/java/me/meinmex/serversystem/serversystem/ServerSystem.java
+++ src/main/java/me/meinmex/serversystem/serversystem/ServerSystem.java
@@ -1,17 +1,25 @@
 package me.meinmex.serversystem.serversystem;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ServerSystem extends JavaPlugin {
 
+    public static String Version = "0.0.1#DEVELOPING";
+    public static String Name = "ServerSystem";
+    public static String Author = "MeinMex#5023";
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getConsoleSender().sendMessage("§aServerSystem enabled!");
+        Bukkit.getConsoleSender().sendMessage("§aVersion: " + Version);
+        Bukkit.getConsoleSender().sendMessage("§aAuthor: " + Author);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getConsoleSender().sendMessage("§aServerSystem disabled!");
+        Bukkit.getConsoleSender().sendMessage("§aVersion: " + Version);
+        Bukkit.getConsoleSender().sendMessage("§aAuthor: " + Author);
     }
 }
